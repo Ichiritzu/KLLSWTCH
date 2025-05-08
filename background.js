5b31d6e49350c20adfaf71657867b3b01ffd31ca
Andrew Ingram <andrewingram1426@gmail.com>
2025-05-08 10:24:12 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('KLLSWTCH installed.');
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "clearNow") {
+    chrome.browsingData.remove({
+      since: Date.now() - 60 * 60 * 1000
+    }, {
+      "cookies": true,
+      "localStorage": true,
+      "cache": true
+    }, () => {
+      console.log("KLLSWTCH: Data manually cleared.");
+      sendResponse({ status: "success" });
+    });
+    return true; // Required to use sendResponse asynchronously
+  }
 });
